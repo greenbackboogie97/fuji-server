327c688b11efd89e175e7106cb6c2a4ff712a145
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-07-27 19:36:23 +0300

    cookie fix 3

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -13,8 +13,7 @@ const sendToken = (user, status, message, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production' ? true : false,
-    SameSite: 'none',
+    secure: process.env.NODE_ENV === 'production',
   };
 
   res.cookie('jwt', token, cookieOptions);
