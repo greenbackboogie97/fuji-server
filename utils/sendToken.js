4f2a1469d92d61368a9525386fabe1cf8fca1665
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-07-28 12:25:41 +0300

    cookies are not http only anymore

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -12,7 +12,6 @@ const sendToken = (user, status, message, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
   };
 
