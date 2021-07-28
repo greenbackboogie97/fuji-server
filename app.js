14455413c7b029f63b4ea966a41d9954ad3a6931
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-07-28 13:14:06 +0300

    cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors(corsOptions}));
+app.use(cors(corsOptions));
 
 // Security HTTP Headers
 app.use(helmet());
