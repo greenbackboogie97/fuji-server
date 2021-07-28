7f775c9444a265a8928df48a4a9f59480c7d1145
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-07-28 12:29:42 +0300

    cookie expire format change

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -11,7 +11,7 @@ const sendToken = (user, status, message, res) => {
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    ).toUTCString(),
     secure: process.env.NODE_ENV === 'production',
   };
 
