70b09ca422a3928951f785c48b4c6f346e19b8a4
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-08-08 22:43:53 +0300

    .

--- routes/mediaRouter.js
+++ routes/mediaRouter.js
@@ -4,3 +4,5 @@ const authController = require('../controllers/authController');
 const mediaController = require('../controllers/mediaController');
 
 Router.post('/', authController.protect, mediaController.uploadMedia);
+
+module.exports = Router;
