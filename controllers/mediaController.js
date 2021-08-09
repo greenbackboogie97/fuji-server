718ddf2c86e0f7eac639a08a254d4e3f537c30e8
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-08-09 12:20:40 +0300

    .

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,3 +1,4 @@
+const User = require('../models/UserModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const { cloudinary } = require('../utils/cloudinary');
@@ -24,11 +25,15 @@ exports.uploadMedia = catchAsync(async (req, res, next) => {
 
 exports.getMediaByID = catchAsync(async (req, res, next) => {
   const id = req.params.id;
+  const user = await User.findById(id);
+  if (!user)
+    return next(new AppError('Provided ID does not match any existing user.'));
 
   const fetch = await cloudinary.search.expression(id).execute();
+  const URL = fetch.resources[0].secure_url;
 
   res.status(200).json({
     status: 'success',
-    data: fetch,
+    URL,
   });
 });
