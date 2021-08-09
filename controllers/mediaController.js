50bb2b0bbb0f5f75b671e9ee783751605169af32
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-08-09 10:59:29 +0300

    .

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,11 +1,22 @@
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const { cloudinary } = require('../utils/cloudinary');
 
 exports.uploadMedia = catchAsync(async (req, res, next) => {
-  const file = req.body.data;
+  const fileURL = req.body.data;
+  if (!fileURL)
+    return next(
+      new AppError('The server did not recive a file URL to upload.', 400)
+    );
+
+  const upload = await cloudinary.uploader.upload(fileURL, {
+    upload_preset: 'fuji',
+  });
+
   res.status(200).json({
     status: 'success',
     data: {
-      file,
+      upload,
     },
   });
 });
