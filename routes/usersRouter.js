595de533d62cef3701a1200c55675aa52d6f95e4
greenbackboogie97 <greenbackboogie@tutamail.com>
2021-08-21 18:17:07 +0300

    .

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,7 +4,7 @@ const usersController = require('../controllers/usersController');
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/signin', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
